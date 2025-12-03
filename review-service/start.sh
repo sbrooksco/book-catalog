30490a35600b33d9e3b3712dfbd44a54078f4a29
Stephen Brooks <brooks.stephen.r@gmail.com>
2025-12-03 15:40:02 -0700

    Add start scripts.

---
+++ review-service/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Convert Render's postgres:// URL to JDBC format
+if [ -n "$DATABASE_URL" ]; then
+  export DATABASE_URL=$(echo $DATABASE_URL | sed 's/postgres:\/\//jdbc:postgresql:\/\//')
+fi
+
+# Start the application
+exec java -jar review-service.jar server config.yaml
